845e4b0092987a94bcdd20f0e6cd865fc0047eb9
lmgaspa <@luhmgasparetto@gmaail.com>
2025-01-08 07:32:51 -0300

    fix

--- frontend/karma.config.js
+++ frontend/karma.config.js
@@ -1,25 +1,17 @@
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    frameworks: ['jasmine'], // Ou mocha, dependendo do projeto
+    files: ['src/**/*.spec.js'],
     browsers: ['ChromeHeadless'],
-    reporters: ['progress', 'coverage'], // Include 'coverage' in the reporters
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/', // Output folder
-      subdir: '.',
-      reporters: [
-        { type: 'html', subdir: '.' },
-        { type: 'lcovonly', subdir: '.' },
-        { type: 'text-summary' }
-      ]
-    },
     singleRun: true,
-    restartOnFileChange: false
+    reporters: ['progress', 'junit', 'coverage'],
+    coverageReporter: {
+      type: 'lcov', // Formato lcov para SonarCloud
+      dir: 'coverage/',
+    },
+    junitReporter: {
+      outputDir: 'coverage/',
+      outputFile: 'junit.xml',
+    },
   });
 };
