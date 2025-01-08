675ea383396154f708098459d701f36e388ba9cd
lmgaspa <@luhmgasparetto@gmaail.com>
2025-01-08 05:18:42 -0300

    fix

--- frontend/karma.config.js
+++ frontend/karma.config.js
@@ -1,37 +1,25 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    reporters: ['progress', 'coverage'], // Include 'coverage' in the reporters
     coverageReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
+      type: 'lcov',
+      dir: 'coverage/', // Output folder
+      subdir: '.',
       reporters: [
         { type: 'html', subdir: '.' },
         { type: 'lcovonly', subdir: '.' },
         { type: 'text-summary' }
-      ],
-      fixWebpackSourcePaths: true
+      ]
     },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    browserDisconnectTimeout: 100000, // Increase timeout for disconnection
-    browserDisconnectTolerance: 5, // Retry 5 times before failing
-    browserNoActivityTimeout: 100000, // Increase inactivity timeout
-    captureTimeout: 120000, // Increase capture timeout for long-running tests
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: false
   });
 };
