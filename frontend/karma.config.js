93b2daee5731e13ea645054431a86f16fa19e702
lmgaspa <@luhmgasparetto@gmaail.com>
2025-01-09 00:26:11 -0300

    Deletando arquivos package-lock.json e .gitlab-ci.yml da raiz

--- frontend/karma.config.js
+++ frontend/karma.config.js
@@ -6,45 +6,15 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('karma-junit-reporter'),
-      require('karma-sonarqube-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false, // mantém os resultados visíveis na interface de relatório
+      clearContext: false,  // Deixa os resultados visíveis no navegador
     },
-    coverageReporter: {
-      dir: 'reports/coverage',
-      subdir: '.',
-      reporters: [
-        { type: 'html' },  // Gera relatório HTML para fácil análise
-        { type: 'text-summary' },  // Mostra resumo de cobertura no terminal
-        { type: 'cobertura', file: 'coverage.xml' },  // Cobertura para GitLab
-        { type: 'lcovonly', file: 'coverage.lcov.info' },  // LCOV para SonarCloud
-      ],
-    },
-    junitReporter: {
-      outputDir: 'reports/junit',
-      outputFile: 'test-results.xml',
-      useBrowserName: false,
-    },
-    sonarQubeReporter: {
-      basePath: 'src',
-      filePattern: '**/*spec.ts',
-      outputDir: 'reports/sonar',
-      encoding: 'utf-8',
-      legacyMode: false,
-      reportName: 'sonar-report.xml',
-    },
-    reporters: ['progress', 'kjhtml', 'junit', 'coverage', 'sonarqube'],
-    browsers: ['ChromeHeadless'], // Apenas modo headless para CI/CD
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
-      },
-    },
-    singleRun: true,  // Garante que execute e finalize no ambiente de CI/CD
-    restartOnFileChange: false,  // Não reinicia ao alterar arquivos em ambiente CI/CD
+    reporters: ['progress', 'kjhtml'],  // Remove "coverage"
+    port: 9876,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: true,
   });
 };
