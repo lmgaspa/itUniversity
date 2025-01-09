8c26543f3f2af14d2d6e482612c828d68470a5a2
lmgaspa <@luhmgasparetto@gmaail.com>
2025-01-08 22:29:14 -0300

    fix

--- frontend/karma.config.js
+++ frontend/karma.config.js
@@ -1,4 +1,4 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -8,32 +8,43 @@ module.exports = function(config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('karma-junit-reporter'),
-      require('karma-sonarqube-execution-reporter'),
+      require('karma-sonarqube-reporter'),
     ],
     client: {
-      clearContext: false, // deixa os resultados visíveis no navegador
+      clearContext: false, // mantém os resultados visíveis na interface de relatório
     },
     coverageReporter: {
-      dir: 'reports',
+      dir: 'reports/coverage',
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },  // Relatório no terminal (stdout)
-        { type: 'cobertura', file: 'ng-coverage.cobertura.xml' },  // GitLab coverage
-        { type: 'lcovonly', file: 'ng-coverage.lcov.info' },  // SonarQube
+        { type: 'html' },  // Gera relatório HTML para fácil análise
+        { type: 'text-summary' },  // Mostra resumo de cobertura no terminal
+        { type: 'cobertura', file: 'coverage.xml' },  // Cobertura para GitLab
+        { type: 'lcovonly', file: 'coverage.lcov.info' },  // LCOV para SonarCloud
       ],
     },
     junitReporter: {
-      outputDir: 'reports',
-      outputFile: 'ng-test.xunit.xml',
+      outputDir: 'reports/junit',
+      outputFile: 'test-results.xml',
       useBrowserName: false,
     },
-    sonarQubeExecutionReporter: {
-      outputDir: 'reports',
-      outputFile: 'ng-test.sonar.xml',
+    sonarQubeReporter: {
+      basePath: 'src',
+      filePattern: '**/*spec.ts',
+      outputDir: 'reports/sonar',
+      encoding: 'utf-8',
+      legacyMode: false,
+      reportName: 'sonar-report.xml',
     },
-    reporters: ['progress', 'kjhtml', 'junit', 'coverage', 'sonarqubeExecution'],
-    browsers: ['Chrome', "ChromeHeadless"],  // Executa em modo headless
-    singleRun: true,  // Executa os testes e fecha
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage', 'sonarqube'],
+    browsers: ['ChromeHeadless'], // Apenas modo headless para CI/CD
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    singleRun: true,  // Garante que execute e finalize no ambiente de CI/CD
+    restartOnFileChange: false,  // Não reinicia ao alterar arquivos em ambiente CI/CD
   });
 };
