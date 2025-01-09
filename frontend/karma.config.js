9893c0eadc028f178c7f5b1b3c4f05aedc15bb64
lmgaspa <@luhmgasparetto@gmaail.com>
2025-01-09 06:03:48 -0300

    Deletando arquivos package-lock.json e .gitlab-ci.yml da raiz

--- frontend/karma.config.js
+++ frontend/karma.config.js
@@ -4,23 +4,30 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage'), // Adicionando o plugin de coverage
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false,  // Deixa os resultados visíveis no navegador
+      jasmine: {},
+      clearContext: false // deixa os resultados visíveis no navegador
     },
-    reporters: ['progress', 'kjhtml'],  //
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'), // Pasta onde os relatórios serão gerados
+      subdir: '.',
+      reporters: [
+        { type: 'html' },  // Gera relatório em HTML (pasta /coverage)
+        { type: 'text-summary' } // Gera um resumo no terminal
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'], // Adiciona coverage aos reports
     port: 9876,
-    browsers: ['ChromeHeadless'],
-    customLaunchers: {
-      ChromeHeadlessWindows: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-gpu', '--disable-extensions', '--remote-debugging-port=9222', '--headless'],
-      }
-    },
-    singleRun: true,
-    restartOnFileChange: true,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Firefox'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
