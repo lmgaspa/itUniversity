e3a1939af788d4c6b02820407f638cdab7821e70
lmgaspa <luhmgasparetto@gmail.com>
2025-05-12 05:46:50 -0300

    fix

---
+++ frontend/src/polyfills.ts
@@ -0,0 +1,12 @@
+// src/polyfills.ts
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Importa os polyfills do Angular (opcional, dependendo do seu navegador alvo)
+import 'zone.js';  // Necessário para o Angular
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
